385fd34b43e919f6ec5abc3f9428064fd6e1dcc8
jonasbay <jonasbay@hotmail.dk>
2020-04-16 09:43:03 +0200

    -Added Course and User

---
+++ StudievenDK/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudievenDK.Models
+{
+    public class Course
+    {
+        [Key] public string CourseName { get; set; }
+        [ForeignKey("Faculty")] public string Faculty { get; set; }
+    }
+}
