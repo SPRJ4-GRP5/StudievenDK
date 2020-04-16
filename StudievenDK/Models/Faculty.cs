fa709213558d5c4f90c0ed02c2a5f1121612c053
Thanh Tieu Pham <au479218@uni.au.dk>
2020-04-16 09:47:48 +0200

    case, faculty and term pushed to master branch (DATABASE)

---
+++ StudievenDK/Models/Faculty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudievenDK.Models
+{
+    public class Faculty
+    {
+        [Key]
+        public string FacultyId { get; set; }
+    }
+}
