45fe0ea45f87d4752b746307b4c04d86a625fceb
Thanh Tieu Pham <au479218@uni.au.dk>
2020-04-23 11:59:14 +0200

    added about us view

--- StudievenDK/Controllers/HomeController.cs
+++ StudievenDK/Controllers/HomeController.cs
@@ -28,6 +28,12 @@ namespace StudievenDK.Controllers
             return View();
         }
 
+        public IActionResult AboutStudieven()
+        {
+            return View();
+
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
