784e9e4405fe27f25c84b58bf9f9434b97758340
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-23 13:43:59 +0200

    begyndt på egne sager view

--- StudievenDK/wwwroot/js/site.js
+++ StudievenDK/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const detailView = document.querySelector('.detail');
+const detailTitle = document.querySelector('.detail-title');
+const masterItems = document.querySelectorAll('.master-item');
+function select(selected) {
+    //Remove active class from all master-items
+    for (var item of masterItems) {
+        item.classList.remove('active-item');
+    }
+    //Make selected tab active
+    selected.classList.add('active-item');
+    //Set the content of the detail to the innerHTML of the selected item
+    detailTitle.innerHTML = selected.innerHTML;
+
+}
