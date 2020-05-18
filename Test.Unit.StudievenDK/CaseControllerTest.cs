af55f647cd0e231a6d4b2a79e3f1ee675d9c1533
Thanh Tieu Pham <au479218@uni.au.dk>
2020-05-18 09:14:57 +0200

    setting test up

--- Test.Unit.StudievenDK/CaseControllerTest.cs
+++ Test.Unit.StudievenDK/CaseControllerTest.cs
@@ -2,28 +2,37 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using StudievenDK.Controllers;
 using StudievenDK.Data;
 using NSubstitute;
+using StudievenDK.Models;
 
 namespace Test.Unit.StudievenDK
 {
     class CaseControllerTest
     {
+        private ApplicationDbContext _applicationDbContext;
+        private IWebHostEnvironment _webHostEnvironment;
+        private CasesController _uut;
+
         [SetUp]
         public void Setup()
         {
-            ApplicationDbContext _applicationDbContext = Substitute.For<ApplicationDbContext>();
-            IWebHostEnvironment _webHostEnvironment = Substitute.For<IWebHostEnvironment>();
+            _applicationDbContext = Substitute.For<ApplicationDbContext>();
+            _webHostEnvironment = Substitute.For<IWebHostEnvironment>();
 
-            CasesController _uut = new CasesController(_applicationDbContext, _webHostEnvironment);
+            _uut = new CasesController(_applicationDbContext, _webHostEnvironment);
         }
 
         [Test]
-        public void Test1()
+        public async void Index_Get_View()
         {
-            Assert.Pass();
+            var _result = await _uut.Index();
+
+            Assert.IsInstanceOf(typeof(CasesController), _uut);
         }
     }
 }
